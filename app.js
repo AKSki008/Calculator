dd02c47df189b3c8e916b5ffca9c2c996addade3
AKSki008 <agnieszka.kwiecienskiba@gmail.com>
2023-03-07 12:08:48 +0100

    EDIT app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const BUTTON = document.querySelectorAll(" button ");
+// const BUTTON = document.querySelectorAll(".container button "); //wyszukanie elementów w doc.
+const NUM = document.querySelectorAll(".number");
+console.log(NUM)
 
-console.log(BUTTON)
+let currentIndex;
+
+NUM.forEach((number, index) => {
+    number.addEventListener("click", (e) =>{
+        // alert(NUM.e)
+        currentIndex = index;
+        console.log(currentIndex+1)
+    });
+});
