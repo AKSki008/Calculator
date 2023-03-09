6b11cdc1006654e965965cace6357e7a3551b1eb
AKSki008 <agnieszka.kwiecienskiba@gmail.com>
2023-03-09 12:56:35 +0100

    EDIT:app.js

--- app.js
+++ app.js
@@ -7,16 +7,22 @@ var plus = document.querySelector(".plus ");
 // console.log(NUM);
 let currentIndex;
 
-let downloadNumber = NUM.forEach((number, index) => { //funkcja wywołuje liczby na ekran
+let downloadNumber = new Function ( NUM.forEach((number, index) => { //wywołuje liczby na ekran
     number.addEventListener("click", (e) =>{
         currentIndex = index;
         wynik.innerHTML = NUM[currentIndex].innerHTML;
+        return(wynik.innerHTML);
     });
-});
+})
+);
 
-plus.addEventListener("click", (number) =>{
-    var firstNumber = number;
+
+downloadNumber();
+plus.addEventListener("click", (firstNumber, secondNumber) =>{
+    firstNumber = wynik.innerHTML;
     alert(firstNumber);
-    // var secondNumber =downloadNumber();
-    // console.log(secondNumber);
+    downloadNumber()
+    secondNumber = wynik.innerHTML;
+    alert(secondNumber);
     });
+
