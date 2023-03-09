afd12c6dd8e9d705f2fa228ddde65ede9b006565
AKSki008 <agnieszka.kwiecienskiba@gmail.com>
2023-03-09 09:55:14 +0100

    EDIT: app.js

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const NUM = document.querySelectorAll(".number");
+const NUM = document.querySelectorAll(".number");//wyszukanie elementów w doc.
+var wynik = document.querySelector(".wynik ");
 
-var wynik = document.querySelector(".wynik "); //wyszukanie elementów w doc.
-console.log(wynik);
-console.log(NUM);
+var plus = document.querySelector(".plus ");
+
+// console.log(plus);
+// console.log(NUM);
 let currentIndex;
 
-NUM.forEach((number, index) => {
+let downloadNumber = NUM.forEach((number, index) => { //funkcja wywołuje liczby na ekran
     number.addEventListener("click", (e) =>{
         currentIndex = index;
         wynik.innerHTML = NUM[currentIndex].innerHTML;
-        // console.log(NUM[currentIndex].innerHTML);
     });
 });
+
+plus.addEventListener("click", (number) =>{
+    var firstNumber = number;
+    alert(firstNumber);
+    // var secondNumber =downloadNumber();
+    // console.log(secondNumber);
+    });
